f21ab9c1f53e411c38fdae7dec7a8b9909146e05
linda <linda.zhou@taosales.cc>
2016-02-29 19:13:33 +0800

    改变状态栏颜色，并加入drawlout

--- app/src/main/java/com/linda/toolbardemo/MainActivity.java
+++ app/src/main/java/com/linda/toolbardemo/MainActivity.java
@@ -1,16 +1,70 @@
 package com.linda.toolbardemo;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by zhoulinda on 16/2/29.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
+
+    @Bind(R.id.mToolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        // App Logo
+        toolbar.setLogo(R.mipmap.ic_launcher);
+        // Title
+        toolbar.setTitle("My Title");
+        // Sub Title
+        toolbar.setSubtitle("Sub title");
+
+        setSupportActionBar(toolbar);
+
+        // Navigation Icon 要設定在 setSupoortActionBar 才有作用
+        // 否則會出現 back button
+        toolbar.setNavigationIcon(R.mipmap.ab_android);
+        // Menu item click 的監聽事件一樣要設定在 setSupportActionBar 才有作用
+        toolbar.setOnMenuItemClickListener(onMenuItemClick);
+    }
+
+    private Toolbar.OnMenuItemClickListener onMenuItemClick = new Toolbar.OnMenuItemClickListener() {
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            String msg = "";
+            switch (menuItem.getItemId()) {
+                case R.id.action_edit:
+                    msg += "Click edit";
+                    break;
+                case R.id.action_share:
+                    msg += "Click share";
+                    break;
+                case R.id.action_settings:
+                    msg += "Click setting";
+                    break;
+            }
+
+            if(!msg.equals("")) {
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+            return true;
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
     }
 }
