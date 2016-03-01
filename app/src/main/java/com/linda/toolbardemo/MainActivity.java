9ca3313af7241c3781d13e99796e5850a4ac7c4f
linda <linda.zhou@taosales.cc>
2016-03-01 12:49:51 +0800

    增加pageslidingTabStrip和ViewPager并填充内容

--- app/src/main/java/com/linda/toolbardemo/MainActivity.java
+++ app/src/main/java/com/linda/toolbardemo/MainActivity.java
@@ -1,35 +1,34 @@
 package com.linda.toolbardemo;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
-
-import butterknife.Bind;
-import butterknife.ButterKnife;
 
 /**
  * Created by zhoulinda on 16/2/29.
  */
 public class MainActivity extends AppCompatActivity {
 
-    @Bind(R.id.mToolbar)
-    Toolbar toolbar;
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
+    private PagerSlidingTabStrip mPagerSlidingTabStrip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ButterKnife.bind(this);
 
         initView();
 
@@ -37,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView(){
 //        StatusBarCompat.compat(this,R.color.colorPrimary);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.mToolbar);
         // App Logo
         toolbar.setLogo(R.mipmap.ic_launcher);
         // Title
@@ -63,6 +63,36 @@ public class MainActivity extends AppCompatActivity {
                 R.string.drawer_close);
         mDrawerToggle.syncState();
         mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        mPagerSlidingTabStrip = (PagerSlidingTabStrip)findViewById(R.id.tabs);
+        initTabsValue();//配置PagerSlidingTabStrip
+        ViewPager viewPager = (ViewPager)findViewById(R.id.pager);
+        viewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+        mPagerSlidingTabStrip.setViewPager(viewPager);
+
+    }
+
+    /**
+     * mPagerSlidingTabStrip默认值配置
+     *
+     */
+    private void initTabsValue() {
+        // 底部游标颜色
+        mPagerSlidingTabStrip.setIndicatorColor(Color.BLUE);
+        // tab的分割线颜色
+        mPagerSlidingTabStrip.setDividerColor(Color.TRANSPARENT);
+        // tab背景
+        mPagerSlidingTabStrip.setBackgroundColor(Color.parseColor("#4876FF"));
+        // tab底线高度
+        mPagerSlidingTabStrip.setUnderlineHeight((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                1, getResources().getDisplayMetrics()));
+        // 游标高度
+        mPagerSlidingTabStrip.setIndicatorHeight((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                5, getResources().getDisplayMetrics()));
+        // 选中的文字颜色
+        mPagerSlidingTabStrip.setSelectedTextColor(Color.WHITE);
+        // 正常文字颜色
+        mPagerSlidingTabStrip.setTextColor(Color.BLACK);
     }
 
     private Toolbar.OnMenuItemClickListener onMenuItemClick = new Toolbar.OnMenuItemClickListener() {
@@ -89,6 +119,32 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
     };
+
+    /* ***************FragmentPagerAdapter***************** */
+    public class MyPagerAdapter extends FragmentPagerAdapter {
+
+        private final String[] TITLES = { "分类", "主页", "热门推荐", "热门收藏", "本月热榜", "今日热榜", "专栏", "随机" };
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return TITLES[position];
+        }
+
+        @Override
+        public int getCount() {
+            return TITLES.length;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return SuperAwesomeCardFragment.newInstance(position);
+        }
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
